883843f25129cd428c9fe68c872bc201fea068e7
Daria <dariabobina@gmail.com>
2021-08-28 16:26:17 +0200

    add lesson13

---
+++ lesson13.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// if (4 == 4) {
+//     console.log('Ok!');
+// }
+
+if (1) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+
+const num = 50;
+if (num < 49) {
+    console.log('Error')
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ok!')
+}
